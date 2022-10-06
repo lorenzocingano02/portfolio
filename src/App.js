04a4ea8fd5b16dc727c88d0d46b77dbd9ed15536
Lorenzo Cingano <github@lorenzocingano.it>
2022-10-06 19:27:19 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+    FaGithub,
+    FaInstagram,
+    FaTelegram,
+    FaWhatsapp
+} from "react-icons/fa";
+import cat from './cat.png'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className={"App-container"}>
+            <div className={"Home-content-container"}>
+                <div className={"Logo-container"}>
+                    <h1>Lorenzo</h1>
+                    <h1>Cingano</h1>
+                </div>
+                <div className={"Separator"}></div>
+                <ul className={"Home-list"}>
+                    <li>
+                        <a href={"#"}>About</a>
+                    </li>
+                    <li>
+                        <a href={"#"}>Pricing</a>
+                    </li>
+                    <li>
+                        <a href={"#"}>Experience</a>
+                    </li>
+                    <li>
+                        <a href={"#"}>Portfolio</a>
+                    </li>
+                    <li>
+                        <a href={"#"}>Contact me</a>
+                    </li>
+                </ul>
+                <div className={"Socials-container"}>
+                    <a href={"#"}>
+                        <FaInstagram/>
+                    </a>
+                    <a href={"#"}>
+                        <FaTelegram/>
+                    </a>
+                    <a href={"#"}>
+                        <FaWhatsapp/>
+                    </a>
+                    <a href={"#"}>
+                        <FaGithub/>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </>
   );
 }
 
